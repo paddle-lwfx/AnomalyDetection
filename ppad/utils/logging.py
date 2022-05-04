9df22dc04f7ea585a8bf0f17d8ed75add6da9852
littletomatodonkey <dazhiningsibuqu@163.com>
2022-05-04 06:52:13 +0000

    init the repo

--- ppad/utils/logging.py
+++ ppad/utils/logging.py
@@ -26,7 +26,7 @@ logger_initialized = {}
 
 
 @functools.lru_cache()
-def get_logger(name='ppocr', log_file=None, log_level=logging.DEBUG):
+def get_logger(name='ppad', log_file=None, log_level=logging.DEBUG):
     """Initialize and get a logger by name.
     If the logger has not been initialized, this method will initialize the
     logger by adding one or two handlers, otherwise the initialized logger will
